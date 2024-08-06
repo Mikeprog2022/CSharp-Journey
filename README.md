0c8ce1d39119d812eb95e4761667516e9d8c65cc
Mikeprog2022 <mikeprog22@gmail.com>
2024-08-06 14:01:00 +0300

    Relearning the basics

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-"# C#_Journey" 
+# C#_Journey
+This is my journey to becoming a C# pro to aid in my career pursuit as a .NET developer.
+
+## Overview
+These are console apps created in VS studio.
+To run them install visual studio, navigate to a project folder, access the terminal and run the project using dotnet run.
+
+### Things learnt:
+
+
+
---
+++ cSharpBasics/Program.cs
@@ -0,0 +1,40 @@
+﻿Console.WriteLine("Hello, World!");
+
+Console.WriteLine(200);
+
+Console.Write("This doesn't add a new line");
+
+//using System;
+
+// uses Main method as entry point of the application
+
+// public class Basics{
+//     public static void Main(string[] args){
+//         Console.WriteLine("This isn't using top level statements");
+//         System.Console.WriteLine("This isn't using the system namespace");
+//     }
+// }
+
+string stringVariableOne;
+ stringVariableOne = "First Variable";
+
+string stringVariableTwo = "Second Variable";
+
+char charVariableOne = 'a';
+
+//charVariableOne = 'bs'; to many characters
+
+int intVariableOne = 0;
+
+double decimalVariableOne = 1.2;
+
+float decimalVariableTwo = 1.23f; // Less storage less accurate/precise
+
+decimal decimalVariableThree = 1.23m; // More storage more precise
+
+bool boolVariableOne = false;
+
+++intVariableOne; //adds then returns
+
+intVariableOne++; //returns then adds
+
